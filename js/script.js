2dc45c44c5b4ce3e805c90a6f81c2de6ca2aabb9
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-06-15 14:59:07 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// 1. Creare un array di oggetti. Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+
+
+
+
+
+// 2. Crea un array di 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+
+
+
+// 3. Crea un array di 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine
